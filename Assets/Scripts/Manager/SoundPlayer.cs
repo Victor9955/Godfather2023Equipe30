931ceb3ffea14c05ecc9efac6f59b39f4330197b
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2023-09-08 11:52:56 +0200

    [Sound]

---
+++ Assets/Scripts/Manager/SoundPlayer.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+class MySound
+{
+    public AudioClip clip;
+    public float volume;
+}
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField] GameEventHandler eventHandler;
+    [SerializeField] AudioSource listener;
+    [SerializeField] List<MySound> sounds;
+
+    void Start()
+    {
+        eventHandler.InitCard += PlayOnInitCard;
+        eventHandler.WrongInput += PlayOnWrongKey;
+        eventHandler.FXWinCard += PlayOnRightKey;
+        eventHandler.WinCard += PlayOnNextCard;
+    }
+
+    void OnDestroy()
+    {
+        eventHandler.InitCard -= PlayOnInitCard;
+        eventHandler.WrongInput -= PlayOnWrongKey;
+        eventHandler.FXWinCard -= PlayOnRightKey;
+        eventHandler.WinCard -= PlayOnNextCard;
+    }
+
+    void PlayOnInitCard()
+    {
+        listener.clip = sounds[0].clip;
+        listener.volume = sounds[0].volume;
+        listener.Play();
+    }
+    void PlayOnWrongKey()
+    {
+        listener.clip = sounds[1].clip;
+        listener.volume = sounds[1].volume;
+        listener.Play();
+    }
+    void PlayOnRightKey()
+    {
+        listener.clip = sounds[2].clip;
+        listener.volume = sounds[2].volume;
+        listener.Play();
+    }
+    void PlayOnNextCard()
+    {
+        listener.clip = sounds[3].clip;
+        listener.volume = sounds[3].volume;
+        listener.Play();
+    }
+}
+
+
+
+
